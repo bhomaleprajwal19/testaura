008238ee002beb034538ab8119aa259f876d19c2
Bhomale Prajwal <bhomaleprajwal19@gmail.com>
2025-08-03 17:56:07 +0530

    leaderboard ui fixing

--- src/pages/leaderboard.jsx
+++ src/pages/leaderboard.jsx
@@ -29,7 +29,7 @@ const Leaderboard = () => {
             <div className="absolute inset-0 bg-[linear-gradient(to_right,#000_1px,transparent_1px),linear-gradient(to_bottom,#000_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)] z-0"></div>
 
             {/* Navbar */}
-            <div className="relative z-10">
+            <div className="relative z-50">
                 <Navbar />
             </div>
 
